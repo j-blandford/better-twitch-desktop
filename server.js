10810ffd194b6977a97d6bdd58c1973da76b6408
James Blandford <icy.moons@gmail.com>
2017-08-22 14:03:35 +0100

    Switched to NPM package "pem" for dev server

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
 const express = require('express');
 const fs = require('fs');
+const pem = require('pem');
 const https = require('https');
 const http = require('http');
 
-// setup HTTPS, as our Twitch Electron instance requires it
-var dev_pk  = fs.readFileSync('config/dkey.pem', 'utf8');
-var dev_cert = fs.readFileSync('config/dcert.pem', 'utf8');
-var credentials = {key: dev_pk, cert: dev_cert};
-
-const app = express();
-app.use(express.static('build'));
-
-var httpsServer = https.createServer(credentials, app)
+pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
+    const app = express();
+    app.use(express.static('build'));
+    
+    var httpsServer = https.createServer({key: keys.serviceKey, cert: keys.certificate}, app)
     .listen(8080, function () {
         console.log('[HTTPS] Development server started on http://localhost:8080')
     });
+});
